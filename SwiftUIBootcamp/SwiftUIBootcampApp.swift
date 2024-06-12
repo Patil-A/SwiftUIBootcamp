9ab1e3863af7bd52a8d55d639e1a07f1f1947f5c
Ajay Patil <ajay.patil@iaai.com>
2024-06-12 11:22:54 +0530

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Ajay Patil on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
